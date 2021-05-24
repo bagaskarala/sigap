e6b6b0298df8b311c5e03d8e7be4fb52b4cc530f
esterinac <esterinachristina@gmail.com>
2021-05-24 10:32:04 +0700

    Fix view book receive
    - ubah progress_text jadi lowercase
    - bisa upload dan download pdf berita acara serah terima setelah penerimaan buku selesai
    - delete unused code

--- application/modules/book_receive/views/view/common/input_staff_modal.php
+++ application/modules/book_receive/views/view/common/input_staff_modal.php
@@ -3,9 +3,9 @@ $level = check_level();
 if ($level == 'superadmin' || $level == 'admin_gudang') :
     $progress_text = '';
     if ($progress == 'handover') {
-        $progress_text = 'Serah Terima';
+        $progress_text = 'serah terima';
     } elseif ($progress == 'wrapping') {
-        $progress_text = 'Wrapping';
+        $progress_text = 'wrapping';
     }
 ?>
     <button
@@ -141,7 +141,6 @@ if ($level == 'superadmin' || $level == 'admin_gudang') :
             $(`#modal-select-staff-${progress}`).modal('toggle')
 
 
-            // get data semua reviewer
             $.get("<?= base_url('book_receive/api_get_staff_gudang'); ?>",
                 function(res) {
                     //  inisialisasi select2
@@ -170,7 +169,6 @@ if ($level == 'superadmin' || $level == 'admin_gudang') :
             )
         })
 
-        // pilih reviewer
         $(`#${progress}-progress-wrapper`).on('click', `#btn-select-staff-gudang-${progress}`, function() {
             const $this = $(this);
             const user_id = $(`#staff-gudang-id-${progress}`).val();
@@ -198,7 +196,6 @@ if ($level == 'superadmin' || $level == 'admin_gudang') :
                 },
                 complete: function() {
                     $(`[name=staff-gudang-id-${progress}]`).val(null).trigger('change');
-                    // reload segemen daftar reviewer
                     $(`#staff-gudang-list-wrapper-${progress}`).load(
                         ` #staff-gudang-list-${progress}`);
 
@@ -207,7 +204,6 @@ if ($level == 'superadmin' || $level == 'admin_gudang') :
             });
         });
 
-        // hapus reviewer
         $(`#${progress}-progress-wrapper`).on('click', `.btn-delete-staff-gudang-${progress}`, function() {
             $(this).attr('disabled', 'disabled').html("<i class='fa fa-spinner fa-spin '></i>");
             let id = $(this).attr('data');
@@ -221,7 +217,6 @@ if ($level == 'superadmin' || $level == 'admin_gudang') :
                     showToast(false, err.responseJSON.message);
                 },
                 complete: function() {
-                    // reload segemen daftar reviewer
                     $(`#staff-gudang-list-wrapper-${progress}`).load(
                         ` #staff-gudang-list-${progress}`);
                 },
@@ -230,80 +225,3 @@ if ($level == 'superadmin' || $level == 'admin_gudang') :
     })
     </script>
 <?php endif; ?>
-
-
-<!-- <?php
-        // $level = check_level();
-        // if ($level == 'superadmin' || $level == 'admin_gudang') :
-        //     $progress_text = '';
-        //     if ($progress == 'handover') {
-        //         $progress_text = 'Serah Terima';
-        //     } elseif ($progress == 'wrapping') {
-        //         $progress_text = 'Wrapping';
-        //     }
-        ?>
-<div class="modal fade" id="modal-staff-<?//= $progress; ?>" tabindex="-1" role="dialog"
-    aria-labelledby="modal-staff-<?//= $progress; ?>" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title"> Staff Gudang untuk Progress <?//= $progress_text; ?> </h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form>
-                    <fieldset>
-                        <div class="form-group" id="form-staff-<?//= $progress; ?>">
-                            <label for="staff-<?//= $progress; ?>">Nama Staff Gudang</label>
-                            <input type="text" id="<?//= $progress ?>_staff" class="form-control">
-                        </div>
-                    </fieldset>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button id="btn-submit-staff-<?//= $progress?>" class="btn btn-primary" type="button">Submit</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-<script>
-    $(document).ready(function() {
-        const book_receive_id = '<?//= $book_receive->book_receive_id; ?>';
-        const progress = '<?//= $progress; ?>';
-
-        // submit progress
-        $(`#${progress}-progress-wrapper`).on('click', `#btn-submit-staff-${progress}`, function() {
-            const $this = $(this);
-
-            $this.attr("disabled", "disabled").html("<i class='fa fa-spinner fa-spin '></i>");
-            $.ajax({
-                type: "POST",
-                url: "<?php echo base_url('book_receive/api_update/'); ?>" + book_receive_id,
-                datatype: "JSON",
-                data: {
-                    progress,
-                    [`${progress}_staff`]: $(`#${progress}_staff`).val(),
-                },
-                success: function(res) {
-                    showToast(true, res.data);
-                },
-                error: function(err) {
-                    showToast(false, err.responseJSON.message);
-                },
-                complete: function() {
-                    $this.removeAttr("disabled").html("Submit");
-                    $(`#${progress}-progress-wrapper`).load(` #${progress}-progress`);
-                    $(`#modal-staff-${progress}`).modal('hide');
-                    $('body').removeClass('modal-open');
-                    $('.modal-backdrop').remove();
-                }
-            });
-        });
-    })
-</script>
-<?php //endif; 
-?> -->
