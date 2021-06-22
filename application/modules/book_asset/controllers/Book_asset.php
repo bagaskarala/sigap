ca3ab969d55652cd5ec792abe347338d7c08893e
esterinac <esterinachristina@gmail.com>
2021-06-22 19:02:18 +0700

    Fix book asset hpp

--- application/modules/book_asset/controllers/Book_asset.php
+++ application/modules/book_asset/controllers/Book_asset.php
@@ -173,25 +173,25 @@ class Book_asset extends Warehouse_Controller
                         break;
                     }
                     case 'K': {
-                        $value = ($data->harga*$data->warehouse_present) + 
-                                 ($data->harga*$data->showroom_present) +
-                                 ($data->harga*$data->library_present);
+                        $value = $data->harga*
+                        ($data->warehouse_present + $data->showroom_present + $data->library_present);
                         break;
                     }
                     case 'L': {
-                        $value = $data->harga*$data->warehouse_present*(1-$hpp);
+                        $value = $data->harga*$data->warehouse_present*$hpp;
                         break;
                     }
                     case 'M': {
-                        $value = $data->harga*$data->showroom_present*(1-$hpp);
+                        $value = $data->harga*$data->showroom_present*$hpp;
                         break;
                     }
                     case 'N': {
-                        $value = $data->harga*$data->library_present*(1-$hpp);
+                        $value = $data->harga*$data->library_present*$hpp;
                         break;
                     }
                     case 'O': {
-                        $value = ($data->harga*(1-$hpp))*($data->warehouse_present+$data->showroom_present+$data->library_present);
+                        $value = $data->harga*$hpp*
+                        ($data->warehouse_present+$data->showroom_present+$data->library_present);
                         break;
                     }
                 }
